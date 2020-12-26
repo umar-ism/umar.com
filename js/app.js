076e0b35d2657d62b56d8b79457e0418bf29b32a
umar-ism <umarismaildjanoff@gmail.com>
2020-12-26 10:18:50 +0500

    update

--- js/app.js
+++ js/app.js
@@ -106,15 +106,14 @@ for(let i=0; i<totalNavList; i++){
     const a=navList[i].querySelector("a");
     a.addEventListener("click", function(){
         //remove back section class
-        for(let i=0; i<totalSection; i++){
-            allSection[i].classList.remove("back-section");
-        }
+        removeBackSectionClass();
 
         for(let j=0; j<totalNavList; j++){
             if(navList[j].querySelector("a").classList.contains("active")){
                 //add back section class
+                addBackSectionClass(j);
                 
-                allSection[j].classList.add("back-section");
+                
             }
             navList[j].querySelector("a").classList.remove("active");
 
@@ -130,6 +129,15 @@ for(let i=0; i<totalNavList; i++){
     })
 }
 
+function removeBackSectionClass (){
+    for(let i=0; i<totalSection; i++){
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+function addBackSectionClass(num){
+    allSection[num].classList.add("back-section");
+}
 
 function showSection(element){
     for(let i=0; i<totalSection; i++){
@@ -139,6 +147,24 @@ function showSection(element){
 
     document.querySelector("#"+target).classList.add("active");
 }
+
+function updateNav(element){
+    for(let i=0; i<totalNavList; i++){
+        navList[i].querySelector("a").classList.remove("active");
+        const target=element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+    removeBackSectionClass();
+    addBackSectionClass(sectionIndex);
+})
 
 
 
